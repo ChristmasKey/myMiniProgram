87f2bacc8f9b47b890fb8e5dd8a3764a15adaf69
丁佳男 <jiananst@qq.com>
2023-08-04 00:25:37 +0800

    更新笔记

--- projects/mp_01/pages/index/index.js
+++ projects/mp_01/pages/index/index.js
@@ -1,48 +1,42 @@
 // index.js
-// 获取应用实例
-const app = getApp()
 
+/* 
+    页面的初始数据
+*/
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
+    data: {
+        info: 'hello world',
+        imgSrc: 'https://i0.hdslb.com/bfs/archive/6919912c21a26ab2dbbf3a821815acc90dd07049.jpg@672w_378h_1c_!web-home-common-cover.avif',
+        randomNum1: Math.random() * 10,
+        randomNum2: Math.random().toFixed(2),
+        count: 0
+    },
+
+    // 定义按钮的事件处理函数
+    outerHandler(e) {
+        console.log(e);
+        console.log(e.target);
+        console.log(e.currentTarget);
+    },
+
+    countChange(e) {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+            count: this.data.count + 1
         })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+    },
+
+    countChange2(e) {
+        this.setData({
+            count: this.data.count + e.target.dataset.step
+        })
+    },
+
+    onLoad() {
+    },
+
+    getUserProfile(e) {
+    },
+
+    getUserInfo(e) {
+    }
 })
