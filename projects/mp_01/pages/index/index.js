bf3748e8ceb40d9cef06f11ea7d3c243d86579e4
丁佳男 <jiananst@qq.com>
2023-08-07 01:07:48 +0800

    全局配置

--- projects/mp_01/pages/index/index.js
+++ projects/mp_01/pages/index/index.js
@@ -9,7 +9,9 @@ Page({
         imgSrc: 'https://i0.hdslb.com/bfs/archive/6919912c21a26ab2dbbf3a821815acc90dd07049.jpg@672w_378h_1c_!web-home-common-cover.avif',
         randomNum1: Math.random() * 10,
         randomNum2: Math.random().toFixed(2),
-        count: 0
+        count: 0,
+        msg: '你好',
+        type: 1
     },
 
     // 定义按钮的事件处理函数
@@ -31,12 +33,13 @@ Page({
         })
     },
 
-    onLoad() {
+    inputHandler(e) {
+        console.log(e.detail.value);
     },
 
-    getUserProfile(e) {
-    },
-
-    getUserInfo(e) {
+    inputHandler2(e) {
+        this.setData({
+            msg: e.detail.value
+        })
     }
 })
